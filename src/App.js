67cffa18b991328d106a7796bd4f867f4fb66527
Muhammmad Afzaal <muhammad.afzaal@tkxel.io>
2021-12-23 18:14:12 +0500

    * Apply public api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch("https://api.publicapis.org/entries")
+    .then((res) => res.json())
+    .then((result) => {
+      console.log("Result:::::::::::::::::::: ", result)
+    })
+  });
   return (
     <div className="App">
       <header className="App-header">
